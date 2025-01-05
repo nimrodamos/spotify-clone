07e490f53ca92119db454a26489c7d8fbce12000
nimrod amos <nimrodamos100@gmail.com>
2025-01-05 16:59:12 +0200

    refactor: remove unused horizontal scroll functionality from DisplayHome component

--- client/src/components/DisplayHome.tsx
+++ client/src/components/DisplayHome.tsx
@@ -1,21 +1,8 @@
 import { albumsData, songsData } from "@/assets/assets";
 import AlbumItem from "./AlbumItem";
 import SongItem from "./SongItem";
-import { useEffect } from "react";
 
 function DisplayHome() {
-  // The following code snippet is used to add a horizontal scroll to the carousel
-  useEffect(() => {
-    const carousels = document.querySelectorAll(".flex.overflow-auto");
-    carousels.forEach((carousel) => {
-      carousel.addEventListener("wheel", (event) => {
-        const wheelEvent = event as WheelEvent;
-        wheelEvent.preventDefault();
-        carousel.scrollLeft += wheelEvent.deltaY;
-      });
-    });
-  }, []);
-
   return (
     <>
       <div className="mb-4">
