a8c92bf6fc6509d9f1efd6c5129abd79449c4b24
Tal Maman <dreapet41s@gmail.com>
2025-01-09 11:05:12 +0200

    feat: add getAlbumByName endpoint to retrieve album by name

--- server/routes/dbAlbumRoute.js
+++ server/routes/dbAlbumRoute.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
 getAlbums,
 getAlbumById,
+getAlbumByName,
 } from '../controllers/albumController.js';
 
 const router = express.Router();
 
 router.get('/:spotifyAlbumId', getAlbumById);
+router.get('/name/:name', getAlbumByName);
 router.get('/', getAlbums);
 
 export default router;
