756425964ea986f320a326a996da8593970ae059
Tal Maman <dreapet41s@gmail.com>
2025-01-09 11:50:52 +0200

    feat: add conditional rendering for playlists in Sidebar component and update SidebarLinks spacing

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -3,13 +3,23 @@ import { SidebarPlaylistPrompt } from "./SidebarPlaylistPrompt";
 import { SidebarPodcastPrompt } from "./SidebarPodcastPrompt";
 import { SidebarLinks } from "./SidebarLinks";
 import { SidebarLanguageSelector } from "./SidebarLanguageSelector";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUserContext } from "../../Context/UserContext.tsx";
 import SidebarPlaylistAndArtists from "./SidebarPlaylistAndArtists";
 
 const Sidebar: React.FC = () => {
   const [width, setWidth] = useState<number>(100);
   const { user } = useUserContext();
+  const [hasPlaylists, setHasPlaylists] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (user && user.playlists) {
+      setHasPlaylists(true);
+    } else {
+      setHasPlaylists(false);
+    }
+  }, [user]);
+
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
     const startX = e.clientX;
     let lastX = startX;
@@ -37,32 +47,26 @@ const Sidebar: React.FC = () => {
   };
 
   return (
-    <div className="flex h-full">
-      <div
-        className="h-full rounded flex-col mr-2 gap-2 text-textBase hidden lg:flex"
-        style={{
-          width: `${width}%`,
-          backgroundColor: "#1e1e1e",
-          minWidth: "300px",
-          maxWidth: "100%",
-        }}
-      >
-        <div className="bg-backgroundBase h-full rounded">
-          <SidebarHeader />
-          {user && user.playlists ? (
-            <SidebarPlaylistAndArtists />
-          ) : (
-            <>
-              <SidebarPlaylistPrompt />
-              <SidebarPodcastPrompt />
-            </>
-          )}
-          <SidebarLinks />
-          <SidebarLanguageSelector />
-        </div>
+    <div className="flex h-full text-textBase">
+      <div>
+        {hasPlaylists ? (
+          <div className="bg-backgroundBase h-full rounded">
+            <SidebarHeader />
+            {user && user.playlists ? (
+              <SidebarPlaylistAndArtists />
+            ) : (
+              <>
+                <SidebarPlaylistPrompt />
+                <SidebarPodcastPrompt />
+              </>
+            )}
+            <SidebarLinks />
+            <SidebarLanguageSelector />
+          </div>
+        ) : null}
       </div>
       <div
-        className="w-[1%] h-full cursor-col-resize hover:bg-essentialSubdued transition-all duration-300 mr-2"
+        className="w-[1%] h-full rounded cursor-col-resize hover:bg-essentialSubdued transition-all duration-300 ml-2 mr-2"
         onMouseDown={handleMouseDown}
       ></div>
     </div>
