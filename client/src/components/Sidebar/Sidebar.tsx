e4741c80b137b0d625b26b1cac014120dfd9a35d
Tal Maman <dreapet41s@gmail.com>
2025-01-14 11:18:39 +0200

    feat: update Sidebar component with filtering and search functionality

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -3,71 +3,62 @@ import { SidebarPlaylistPrompt } from "./SidebarPlaylistPrompt";
 import { SidebarPodcastPrompt } from "./SidebarPodcastPrompt";
 import { SidebarLinks } from "./SidebarLinks";
 import { SidebarLanguageSelector } from "./SidebarLanguageSelector";
-import { useState, useEffect } from "react";
 import { useUserContext } from "../../Context/UserContext.tsx";
 import SidebarPlaylistAndArtists from "./SidebarPlaylistAndArtists.tsx";
+import { useState } from "react";
 
 const Sidebar: React.FC = () => {
-  const [width, setWidth] = useState<number>(100);
   const { user } = useUserContext();
-  const [hasPlaylists, setHasPlaylists] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (user && user.playlists) {
-      setHasPlaylists(true);
-    } else {
-      setHasPlaylists(false);
-    }
-  }, [user]);
+  const [filter, setFilter] = useState<'playlists' | 'artists' | null>(null);
+  const [sidebarFilter, setSidebarFilter] = useState<'Recents' | 'Recently Added' | 'Alphabetical' | 'Creator'>('Recents');
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
-    const startX = e.clientX;
-    let lastX = startX;
-
-    const onMouseMove = (e: MouseEvent) => {
-      const currentX = e.clientX;
-      const deltaX = currentX - lastX;
-
-      setWidth((prevWidth) => {
-        const newWidth = prevWidth + deltaX;
-        const newWidthPx = (newWidth / 100) * window.innerWidth;
-        return newWidthPx >= 300 ? newWidth : (300 / window.innerWidth) * 100;
-      });
-
-      lastX = currentX;
-    };
-
-    const onMouseUp = () => {
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-    };
-
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
+  const clearFilter = () => {
+    setFilter(null);
+    setSearchQuery("");
   };
 
   return (
-    <div className="flex h-[100%] min-w-[26rem] text-textBase">
+    <div className="flex h-[100%] min-w-[26.5rem] text-textBase">
       <div className="flex-grow">
         <div className="bg-backgroundBase h-full rounded">
-          <SidebarHeader />
+          <SidebarHeader
+            filter={filter}
+            setFilter={setFilter}
+            sidebarFilter={sidebarFilter}
+            setSidebarFilter={setSidebarFilter}
+            isSearchActive={isSearchActive}
+            setIsSearchActive={setIsSearchActive}
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+            clearFilter={clearFilter}
+          />
           <div className="h-fit overflow-auto bg-backgroundBase">
-            {user?.playlists ? (
-              <SidebarPlaylistAndArtists />
-            ) : (
-              <>
-                <SidebarPlaylistPrompt />
-                <SidebarPodcastPrompt />
-                <SidebarLinks />
-                <SidebarLanguageSelector />
-              </>
-            )}
-          </div>
+                      {user?.playlists ? (
+                        <SidebarPlaylistAndArtists
+                          filter={filter}
+                          searchQuery={searchQuery}
+                          sidebarFilter={sidebarFilter}
+                          setFilter={setFilter}
+                          setSidebarFilter={setSidebarFilter}
+                          setSearchQuery={setSearchQuery}
+                          clearFilter={clearFilter}
+                        />
+                      ) : (
+                        <>
+                          <SidebarPlaylistPrompt />
+                          <SidebarPodcastPrompt />
+                          <SidebarLinks />
+                          <SidebarLanguageSelector />
+                        </>
+                      )}
+                    </div>
         </div>
       </div>
       <div
         className="w-[0.1rem] h-full rounded cursor-col-resize hover:bg-essentialSubdued transition-all ml-1 mr-1 duration-300"
-        onMouseDown={handleMouseDown}
       ></div>
     </div>
   );
