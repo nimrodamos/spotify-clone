4c26fd504d5ecd2c628e259fc8721191a45f38b6
Tal Maman <dreapet41s@gmail.com>
2025-01-14 11:05:40 +0200

    Revert "refactor: clean up App component by removing redundant code and adjusting layout"
    This reverts commit 1ddf5293e5bef2b57d36238bf8c49415eb3e2b24.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/Login";
 import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import PasswordRecoveryPage from "./pages/ForgotPassword";
 
 const App = () => {
@@ -19,12 +20,20 @@ const App = () => {
     <UserProvider>
       <div className="h-screen bg-black">
         {!isFullPage && (
+          <div className="h-[9%]">
+        {!isFullPage && (
           <div className="h-[8.5%]">
             <Navbar />
           </div>
         )}
 
         <div className={isFullPage ? "h-full" : "h-[81%]"}>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<LogIn />} />
+              <Route path="/password-reset" element={<PasswordRecoveryPage />} />
+            </Routes>
           <Routes>
             <Route path="*" element={<Main />} />
             <Route path="/signup" element={<SignUp />} />
@@ -33,6 +42,7 @@ const App = () => {
           </Routes>
         </div>
 
+        {!isFullPage && (
         {!isFullPage && (
           <div className="h-[10%]">
             <Player />
