6f2cbeb1edd55f8fd2be8882ac68ea65e42b4b81
Tal Maman <dreapet41s@gmail.com>
2025-01-14 11:06:02 +0200

    Reapply "feat: add NotFoundPage component and update routing to handle 404 errors"
    This reverts commit 67cd831c1c753bec584b9faaf8406c56aded83a1.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/Login";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { Routes, Route, useLocation } from "react-router-dom";
 import PasswordRecoveryPage from "./pages/ForgotPassword";
 
 const App = () => {
@@ -21,8 +20,6 @@ const App = () => {
       <div className="h-screen bg-black">
         {!isFullPage && (
           <div className="h-[9%]">
-        {!isFullPage && (
-          <div className="h-[8.5%]">
             <Navbar />
           </div>
         )}
@@ -34,15 +31,8 @@ const App = () => {
               <Route path="/login" element={<LogIn />} />
               <Route path="/password-reset" element={<PasswordRecoveryPage />} />
             </Routes>
-          <Routes>
-            <Route path="*" element={<Main />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<LogIn />} />
-            <Route path="/password-reset" element={<PasswordRecoveryPage />} />
-          </Routes>
         </div>
 
-        {!isFullPage && (
         {!isFullPage && (
           <div className="h-[10%]">
             <Player />
