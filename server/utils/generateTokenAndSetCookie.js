b050cbcc96489128bd6cf5ba9dd74db83cd9f251
Tal Maman <dreapet41s@gmail.com>
2025-01-15 15:31:06 +0200

    feat: update cookie settings for JWT token to allow lax sameSite and enable client access

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -4,11 +4,12 @@ const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 		expiresIn: "15d",
 	});
+	console.log(token);
 
 	res.cookie("jwt", token, {
-		httpOnly: true,
+		httpOnly: false,
 		maxAge: 15 * 24 * 60 * 60 * 1000,
-		sameSite: "strict",
+		sameSite: "lax",
 	});
 
 	return token;
