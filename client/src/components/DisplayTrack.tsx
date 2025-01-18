017852275012ca5b74575f862e5a6a3539800ce6
nimrod amos <nimrodamos100@gmail.com>
2025-01-18 14:06:43 +0200

    refactor: implement dynamic background color based on album cover in DisplayTrack component

--- client/src/components/DisplayTrack.tsx
+++ client/src/components/DisplayTrack.tsx
@@ -6,6 +6,7 @@ import { api } from "@/api";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { FaCheckCircle } from "react-icons/fa";
 import { BiPlusCircle } from "react-icons/bi";
+import { getDominantColor } from "@/lib/getDominantColor";
 
 const DisplayTrack: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -15,6 +16,7 @@ const DisplayTrack: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const [added, setAdded] = useState<boolean>(false);
+  const [background, setBackground] = useState<string>("#121212");
 
   useEffect(() => {
     const fetchTrack = async () => {
@@ -38,6 +40,14 @@ const DisplayTrack: React.FC = () => {
       fetchTrack();
     }
   }, [id, tracks, contextLoading]);
+
+  useEffect(() => {
+    if (track?.albumCoverUrl) {
+      getDominantColor(track.albumCoverUrl).then((color) => {
+        setBackground(color);
+      });
+    }
+  }, [track]);
 
   useEffect(() => {
     const fetchArtist = async () => {
@@ -64,49 +74,59 @@ const DisplayTrack: React.FC = () => {
   if (!artist) return <p>Artist not found</p>;
 
   return (
-    <div className="bg-black text-white h-screen p-8">
-      <div className="flex gap-8 items-center">
-        <img
-          src={track.albumCoverUrl}
-          alt={track.name}
-          className="w-[200px] h-[200px] object-cover rounded-md"
-        />
-        <div>
-          <p className="text-sm text-gray-400">Song</p>
-          <h1 className="text-6xl font-bold mb-4">{track.name}</h1>
-          <p className="text-lg">
-            {track.artist} • {track.album} •{" "}
-            {Math.floor(track.durationMs / 60000)}:
-            {((track.durationMs % 60000) / 1000).toFixed(0).padStart(2, "0")}
-          </p>
+    <div className="max-w-screen-lg mx-auto">
+      <div
+        className="p-5"
+        style={{
+          background: `linear-gradient(to bottom, ${background}, #121212)`,
+        }}
+      >
+        <div className="flex gap-8 items-center">
+          <img
+            src={track.albumCoverUrl}
+            alt={track.name}
+            className="w-[200px] h-[200px] object-cover rounded-md"
+          />
+          <div>
+            <p className="text-sm text-gray-400">Song</p>
+            <h1 className="text-6xl font-bold mb-4">{track.name}</h1>
+            <p className="text-lg">
+              {track.artist} • {track.album} •{" "}
+              {Math.floor(track.durationMs / 60000)}:
+              {((track.durationMs % 60000) / 1000).toFixed(0).padStart(2, "0")}
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="mt-8 flex gap-4 items-center">
-        <AiFillPlayCircle size={70} color="LimeGreen" />
-        <button onClick={() => setAdded(!added)} className="focus:outline-none">
-          {added ? (
-            <FaCheckCircle size={32} color="LimeGreen" />
-          ) : (
-            <BiPlusCircle size={32} />
-          )}
-        </button>
-        <div className="text-white text-2xl cursor-pointer">&#8230;</div>
-      </div>
-      <div className="mt-8 flex items-center gap-4">
-        <img
-          src={artist.images[0].url}
-          alt={track.name}
-          className="w-[70px] h-[70px] object-cover rounded-full"
-        />
-        <div className="flex flex-col">
-          <h1 className="text-l font-bold ">artist</h1>
-          <h1 className="text-l font-bold ">{track.artist}</h1>
+        <div className="mt-8 flex gap-4 items-center">
+          <AiFillPlayCircle size={70} color="LimeGreen" />
+          <button
+            onClick={() => setAdded(!added)}
+            className="focus:outline-none"
+          >
+            {added ? (
+              <FaCheckCircle size={32} color="LimeGreen" />
+            ) : (
+              <BiPlusCircle size={32} />
+            )}
+          </button>
+          <div className="text-white text-2xl cursor-pointer">&#8230;</div>
         </div>
-      </div>
-      <div className="mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Recommended</h2>
-        {/* Render recommended tracks here if available */}
+        <div className="mt-8 flex items-center gap-4">
+          <img
+            src={artist.images[0].url}
+            alt={track.name}
+            className="w-[70px] h-[70px] object-cover rounded-full"
+          />
+          <div className="flex flex-col">
+            <h1 className="text-l font-bold ">artist</h1>
+            <h1 className="text-l font-bold ">{track.artist}</h1>
+          </div>
+        </div>
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-4">Recommended</h2>
+          {/* Render recommended tracks here if available */}
+        </div>
       </div>
     </div>
   );
